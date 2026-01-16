a7387d9a56993017f71152559df87e6d8704dc36
Manya Awasthi <awasthimanya79@gmail.com>
2026-01-16 14:04:51 +0530

    complete auth flow and user-linked resume APIs

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,17 +1,19 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const protect = async (req, res, next) => {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
 
-    if(!token){
-        return res.status(401).json({ message: "Unauthorized" });
-    }
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    try {
-        
-        const decoded =  jwt.verify(token, process.env.JWT_SECRET);
-    } catch (error) {
-        
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
 
-}
+export default protect;
