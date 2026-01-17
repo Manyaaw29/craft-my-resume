ab6fce2dfcf17b8436a33be3ae9c8ffed5b07d9c
Manya Awasthi <awasthimanya79@gmail.com>
2026-01-17 13:01:38 +0530

    Gemini AI enhancement and extracting data from the uploaded resume

---
+++ backend/controllers/openaiController.js
@@ -0,0 +1,173 @@
+//controller for enhancing  a resume's professional summary
+// POST : /api/openai/enhance-professional-summary
+
+import Resume from "../models/Resume.model.js";
+import openai from "../configs/openai.js";
+
+export const enhanceProfessionalSummary = async (req, res) => {
+  try {
+    const { userContent } = req.body;
+    if (!userContent) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const response = await openai.chat.completions.create({
+      model: process.env.OPENAI_MODEL_NAME,
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an expert resume writer specializing in professional summaries. Your task is to create compelling, ATS-friendly professional summaries that are 1-2 sentences long. Highlight key skills, years of experience, and career objectives. Make it impactful and tailored to the user's field. Return only the summary text, no explanations or additional formatting.",
+        },
+        {
+          role: "user",
+          content: userContent,
+        },
+      ],
+    });
+    const enhancedContent = response.choices[0].message.content;
+    return res.status(200).json({ enhancedContent });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for enhancing a resume's job description
+// POST : /api/openai/enhance-job-description
+
+export const enhanceJobDescription = async (req, res) => {
+  try {
+    const { userContent } = req.body;
+    if (!userContent) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const response = await openai.chat.completions.create({
+      model: process.env.OPENAI_MODEL_NAME,
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an expert resume writer specializing in job descriptions. Your task is to create compelling, ATS-friendly job descriptions that are 1-2 sentences long. Highlight key responsibilities, achievements, and skills relevant to the position. Make it impactful and tailored to the user's field. Return only the job description text, no explanations or additional formatting.",
+        },
+        {
+          role: "user",
+          content: userContent,
+        },
+      ],
+    });
+    const enhancedContent = response.choices[0].message.content;
+    return res.status(200).json({ enhancedContent });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for uploading a  resume to the database
+// POST : /api/openai/upload-resume
+
+export const uploadResume = async (req, res) => {
+  try {
+    const { resumeText, title } = req.body;
+    const userId = req.userId;
+
+    if (!resumeText) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const systemPrompt =
+      "You are an expert AI agent that extracts data from resume";
+
+    const userPrompt = `Extract data from this resume : ${resumeText}
+        
+        Provide the data in the following JSON format with no additional explanations or text:
+
+          professional_summary: {
+      type: String,
+      default: "",
+    },
+
+    skills: [{ type: String }],
+
+    languages: [
+      {
+        name: { type: String },
+        proficiency: {
+          type: String,
+          enum: ["Native", "Fluent", "Professional", "Conversational", "Basic"],
+          default: "Fluent",
+        },
+      },
+    ],
+
+    personal_info: {
+      image: { type: String, default: "" },
+      full_name: { type: String, default: "" },
+      profession: { type: String, default: "" },
+      email: { type: String, default: "" },
+      phone: { type: String, default: "" },
+      linkedin: { type: String, default: "" },
+      website: { type: String, default: "" },
+      github: { type: String, default: "" },
+      location: { type: String, default: "" },
+    },
+
+    experience: [
+      {
+        company: { type: String, default: "" },
+        position: { type: String, default: "" },
+        start_date: { type: String, default: "" },
+        end_date: { type: String, default: "" },
+        is_current: { type: Boolean, default: false },
+        description: { type: String, default: "" },
+      },
+    ],
+
+    project: [
+      {
+        name: { type: String, default: "" },
+        description: { type: String, default: "" },
+        link: { type: String, default: "" },
+      },
+    ],
+
+    education: [
+      {
+        institution: { type: String, default: "" },
+        degree: { type: String, default: "" },
+        field: { type: String, default: "" },
+        graduation_date: { type: String, default: "" },
+        score_type: {
+          type: String,
+          enum: ["percentage", "cgpa"],
+          default: "percentage",
+        },
+        score: { type: String, default: "" },
+      },
+    ],
+        
+        `;
+
+    const response = await openai.chat.completions.create({
+      model: process.env.OPENAI_MODEL_NAME,
+      messages: [
+        { role: "system", content: systemPrompt },
+        {
+          role: "user",
+          content: userPrompt,
+        },
+      ],
+      response_format: { type: "json_object" },
+    });
+    const extractedData = response.choices[0].message.content;
+    const parsedData = JSON.parse(extractedData);
+    const newResume = await Resume.create({
+      userId,
+      title,
+      ...parsedData,
+    });
+    res.json({ resumeId: newResume._id });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
--- backend/controllers/resumeController.js
+++ backend/controllers/resumeController.js
@@ -2,60 +2,121 @@
 
 // POST : /api/resumes/create
 
-export const createResume = async (req, res) => {
-    try {
-        const userId = req.userId;
-        const {title} = req.body;
+import  Resume from "../models/Resume.model.js";
+import imageKit from "../configs/imageKit.js";
+import fs from "fs";
 
-        //create new resume 
-        const newResume = await Resume.create({
-            userId,
-            title
-        });
-        //return success message
-        return res.status(201).json({ message: "Resume created successfully", resume: newResume });
-    } catch (error) {
-        return res.status(400).json({ message: error.message });
-    }
-}
+export const createResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { title } = req.body;
+
+    //create new resume
+    const newResume = await Resume.create({
+      userId,
+      title,
+    });
+    //return success message
+    return res
+      .status(201)
+      .json({ message: "Resume created successfully", resume: newResume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
 
 //controller for deleting a resume
 // DELETE : /api/resumes/delete
 
 export const deleteResume = async (req, res) => {
-    try {
-        const userId = req.userId;
-        const {resumeId} = req.params;
-        await Resume.findOneAndDelete({ _id: resumeId, userId });
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
+    await Resume.findOneAndDelete({ _id: resumeId, userId });
 
-        return  res.status(200).json({ message: "Resume deleted successfully" });
-    }catch(error){
-        return res.status(400).json({ message: error.message });
-    }
-
-   
-
-}
+    return res.status(200).json({ message: "Resume deleted successfully" });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
 
 // get user resume by id
 // GET : /api/resumes/get
 
 export const getResumesByUserId = async (req, res) => {
-    try {
-        const userId = req.userId;
-        const {resumeId} = req.params;
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
 
-        const resume = await Resume.findOne({ _id: resumeId, userId });
+    const resume = await Resume.findOne({ _id: resumeId, userId });
 
-        if(!resume){
-            return res.status(404).json({ message: "Resume not found" });
-        }
-        resume.__v = undefined; 
-        resume.createdAt = undefined;
-        resume.updatedAt = undefined;
-        
-        return res.status(200).json({ resume });
-    } catch (error) {
-        return res.status(400).json({ message: error.message });
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
     }
-}
+    resume.__v = undefined;
+    resume.createdAt = undefined;
+    resume.updatedAt = undefined;
+
+    return res.status(200).json({ resume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// get resume by id public
+// GET : /api/resumes/public
+
+export const getPublicResumeById = async (req, res) => {
+  try {
+    const { resumeId } = req.params;
+
+    const resume = await Resume.findOne({ _id: resumeId, public: true });
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    return res.status(200).json({ resume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for updating a resume
+// PUT : /api/resumes/update
+
+export const updateResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId, resumeData, removeBackground } = req.body;
+    const image = req.file;
+
+   
+    let resumeDataCopy = JSON.parse(resumeData);
+
+    if (image) {
+      const imageBufferData = fs.createReadStream(image.path);
+      const response = await imageKit.files.upload({
+        file: imageBufferData,
+        fileName: "resume.png",
+        folder: "user-resumes",
+        transformation: {
+          pre: "w-300,h-300,fo-face,z-0.75" + (removeBackground ? ",e-bgremove" : "")
+        }
+      });
+      
+      resumeDataCopy.personal_info.image = response.url;
+    }
+
+    const resume = await Resume.findOneAndUpdate(
+      { _id: resumeId, userId },
+      resumeDataCopy,
+      { new: true }
+    );
+
+    return res
+      .status(200)
+      .json({ message: "Resume updated successfully", resume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
