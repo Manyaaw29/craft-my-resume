8fd5438d312676c527aebed69ae5db67d1611712
Manya Awasthi <awasthimanya79@gmail.com>
2026-01-21 11:29:33 +0530

    Bug fix in middleware logic

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -9,7 +9,7 @@ const protect = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.userId;
+    req.userId = decoded.userId;
     next();
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized" });
